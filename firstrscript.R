98bd11d454c0d69b33dfe59b0d65a95859f67cfa
Marco Tchernychev <marcotchernychev@gmail.com>
2021-06-20 20:41:58 -0400

    Changed title of header

--- firstrscript.R
+++ firstrscript.R
@@ -3,5 +3,10 @@ library(rvest)
 
 x <- read_html("https://en.wikipedia.org/wiki/UEFA_Euro_2020_squads")
 
-x %>% 
+raw_data <- x %>%
+                  html_elements(".nat-fs-player th") %>%
+                  html_text2() %>%
+                  as_tibble()
+
+write_csv(raw_data, "birthdates.csv")
  
